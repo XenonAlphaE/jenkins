a634bfbd48bd8a99893483bb1e06a71410c351a2
Steve <stevehuytrannd92@gmail.com>
2025-08-29 23:15:44 +0700

    add hyper

--- gitRepos/hyper.groovy
+++ gitRepos/hyper.groovy
@@ -19,6 +19,15 @@ return [
                 MAIN_TWITTER:'@btchyper',
                 OG_PATH:'/img/btchyper/OG.jpeg'
             ],
+            [ 
+                MAIN_DOMAIN:'https://btchype.io/',
+                BACKLINKS_URL:'https://flockez.netlify.app/js/backlinks.json',
+                LOGO_PATH:'/img/btchyper/token.svg',
+                MAIN_GTAG:'G-EV4VMQB5HZ',
+                MAIN_SITENAME:'btchyper',
+                MAIN_TWITTER:'@btchyper',
+                OG_PATH:'/img/btchyper/OG.jpeg'
+            ],
            
         ]
 

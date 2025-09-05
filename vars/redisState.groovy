7981d79712b5b8c8480edaf4002de8447d11d9e8
Steve <stevehuytrannd92@gmail.com>
2025-09-05 15:48:29 +0700

    verify test

--- vars/redisState.groovy
+++ vars/redisState.groovy
@@ -1,10 +1,4 @@
-import redis.clients.jedis.Jedis
 
 def call(String name = "test") {
     return "redisState loaded for ${name}"
-}
-
-class RedisState implements Serializable {
-    String namespace
-    
 }

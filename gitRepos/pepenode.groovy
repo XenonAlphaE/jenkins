94c73f2f2d436bd8710005e7763c8cc5c57786e7
Steve <stevehuytrannd92@gmail.com>
2025-09-13 11:20:54 +0700

    add new repo

--- gitRepos/pepenode.groovy
+++ gitRepos/pepenode.groovy
@@ -27,6 +27,15 @@ return [
                 MAIN_TWITTER:'@pepenode',
                 OG_PATH:'/img/pepenode/OG.jpeg'
             ],
+            [ 
+                MAIN_DOMAIN:'https://pepenode.nl/',
+                BACKLINKS_URL:'https://flockez.netlify.app/js/backlinks.json',
+                LOGO_PATH:'/img/pepenode/token.svg',
+                MAIN_GTAG:'G-DT4375LBL3',
+                MAIN_SITENAME:'pepenode',
+                MAIN_TWITTER:'@pepenode',
+                OG_PATH:'/img/pepenode/OG.jpeg'
+            ],
         ]
 
     ]

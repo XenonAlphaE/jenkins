c4743e79b2dbde7d7cfcacc4334927b0e12b5f6d
Steve <stevehuytrannd92@gmail.com>
2026-01-15 00:09:39 +0700

    remove vps

--- vps.groovy
+++ vps.groovy
@@ -1,11 +1,11 @@
 // vps.groovy
 return [
-    xenon_vps: [
-        vpsCredId    : 'xenon_vps',
-        vpsUser      : 'ubuntu',
-        vpsHost      : '165.154.235.179',
-        webrootBase  : '/var/www/presale'
-    ],
+    // xenon_vps: [
+    //     vpsCredId    : 'xenon_vps',
+    //     vpsUser      : 'ubuntu',
+    //     vpsHost      : '165.154.235.179',
+    //     webrootBase  : '/var/www/presale'
+    // ],
     xenon_vps2: [
         vpsCredId    : 'xenon_vps2',
         vpsUser      : 'root',

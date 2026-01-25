b66fb9f964b423e7412cfb26547538142b5e4247
Steve <stevehuytrannd92@gmail.com>
2026-01-25 13:40:38 +0700

    revert repo

--- nextjs.repos.groovy
+++ nextjs.repos.groovy
@@ -2,13 +2,13 @@
 def repos = []
 repos += load "${env.WORKSPACE}/gitRepos/ico_sonami.groovy"
 
-// repos += load "${env.WORKSPACE}/gitRepos/xenon_lq.groovy"
-// repos += load "${env.WORKSPACE}/gitRepos/xenon_nxs.groovy"
+repos += load "${env.WORKSPACE}/gitRepos/xenon_lq.groovy"
+repos += load "${env.WORKSPACE}/gitRepos/xenon_nxs.groovy"
 
-// repos += load "${env.WORKSPACE}/gitRepos/nxsdefi.groovy"
-// repos += load "${env.WORKSPACE}/gitRepos/liquidchain.groovy"
-// repos += load "${env.WORKSPACE}/gitRepos/supepe.groovy"
-// repos += load "${env.WORKSPACE}/gitRepos/subbd.groovy"
+repos += load "${env.WORKSPACE}/gitRepos/nxsdefi.groovy"
+repos += load "${env.WORKSPACE}/gitRepos/liquidchain.groovy"
+repos += load "${env.WORKSPACE}/gitRepos/supepe.groovy"
+repos += load "${env.WORKSPACE}/gitRepos/subbd.groovy"
 
 
 return repos.collect { repo ->

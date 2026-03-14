59c175dd62097baee123099d01699ef1d85a14f5
Steve <stevehuytrannd92@gmail.com>
2026-03-14 21:14:22 +0700

    build single

--- nextjs.repos.groovy
+++ nextjs.repos.groovy
@@ -1,15 +1,15 @@
 
 def repos = []
 repos += load "${env.WORKSPACE}/gitRepos/ico_pepeto.groovy"
-repos += load "${env.WORKSPACE}/gitRepos/ico_sonami.groovy"
-repos += load "${env.WORKSPACE}/gitRepos/liquidchain.groovy"
-repos += load "${env.WORKSPACE}/gitRepos/nxsdefi.groovy"
+// repos += load "${env.WORKSPACE}/gitRepos/ico_sonami.groovy"
+// repos += load "${env.WORKSPACE}/gitRepos/liquidchain.groovy"
+// repos += load "${env.WORKSPACE}/gitRepos/nxsdefi.groovy"
 
 
-repos += load "${env.WORKSPACE}/gitRepos/xenon_lq.groovy"
-repos += load "${env.WORKSPACE}/gitRepos/xenon_nxs.groovy"
-repos += load "${env.WORKSPACE}/gitRepos/supepe.groovy"
-repos += load "${env.WORKSPACE}/gitRepos/subbd.groovy"
+// repos += load "${env.WORKSPACE}/gitRepos/xenon_lq.groovy"
+// repos += load "${env.WORKSPACE}/gitRepos/xenon_nxs.groovy"
+// repos += load "${env.WORKSPACE}/gitRepos/supepe.groovy"
+// repos += load "${env.WORKSPACE}/gitRepos/subbd.groovy"
 
 
 return repos.collect { repo ->
